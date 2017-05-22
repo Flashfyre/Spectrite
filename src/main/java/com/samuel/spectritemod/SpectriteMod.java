26792b08e3e1e00c85caa6179efb7907d0632ada
Samuel-Harbord <flashfireex@gmail.com>
2017-05-22 15:35:10 -0400

    Fixed missing Diamond Rod model from last update

--- src/main/java/com/samuel/spectritemod/SpectriteMod.java
+++ src/main/java/com/samuel/spectritemod/SpectriteMod.java
@@ -41,7 +41,7 @@ import com.samuel.spectritemod.world.WorldGenSpectrite;
 public class SpectriteMod {
 	public static final String MOD_NAME = "Spectrite Mod";
 	public static final String MOD_ID = "spectritemod";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 
 	@Mod.Instance
 	public static SpectriteMod Instance = new SpectriteMod();
