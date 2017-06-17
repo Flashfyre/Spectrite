b66238e2b5c200da23c74c59c4699d17bf1e9d10
Samuel-Harbord <flashfireex@gmail.com>
2017-06-16 23:14:36 -0400

    Changed version number for Spectrite Healing Orb bug fix
    Might as well make it 1.1.1.1 since 1.1.1 was only just released and
    it's only a single bug fix

--- src/main/java/com/samuel/spectritemod/SpectriteMod.java
+++ src/main/java/com/samuel/spectritemod/SpectriteMod.java
@@ -47,7 +47,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class SpectriteMod {
 	public static final String MOD_NAME = "Spectrite Mod";
 	public static final String MOD_ID = "spectritemod";
-	public static final String VERSION = "1.1.1";
+	public static final String VERSION = "1.1.1.1";
 
 	@Mod.Instance
 	public static SpectriteMod Instance = new SpectriteMod();
