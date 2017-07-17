edadaa3f063697a3690cd766e03b9224d7068f18
Samuel-Harbord <flashfireex@gmail.com>
2017-07-16 21:40:33 -0400

    Fixed Spectrite Chest smelting recipe yielding Spectrite Gems instead of blocks

--- src/main/java/com/samuel/spectritemod/SpectriteMod.java
+++ src/main/java/com/samuel/spectritemod/SpectriteMod.java
@@ -28,7 +28,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class SpectriteMod {
 	public static final String MOD_NAME = "Spectrite Mod";
 	public static final String MOD_ID = "spectritemod";
-	public static final String VERSION = "1.3.0";
+	public static final String VERSION = "1.3.1";
 
 	@Mod.Instance
 	public static SpectriteMod Instance = new SpectriteMod();
--- src/main/java/com/samuel/spectritemod/init/ModCrafting.java
+++ src/main/java/com/samuel/spectritemod/init/ModCrafting.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public final class ModCrafting {
 
 	public static void initCrafting() {
-		GameRegistry.addSmelting(ModBlocks.spectrite_chest, new ItemStack(ModItems.spectrite_gem, 5), 5.0f);
-		GameRegistry.addSmelting(ModBlocks.spectrite_chest_trapped, new ItemStack(ModItems.spectrite_gem, 5), 5.0f);
+		GameRegistry.addSmelting(ModBlocks.spectrite_chest, new ItemStack(ModBlocks.spectrite_block, 5), 5.0f);
+		GameRegistry.addSmelting(ModBlocks.spectrite_chest_trapped, new ItemStack(ModBlocks.spectrite_block, 5), 5.0f);
 	}
 }
